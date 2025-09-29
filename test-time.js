3f5b2e5782a856f3a02984e37009716a72caf8c2
alexmih2018-bit <alexmih2018@gmail.com>
2025-09-29 19:10:08 +0300

    Удален тестовый файл

--- test-time.js
+++
@@ -1,4 +0,0 @@
-console.log('Текущее время:', new Date().toLocaleString('ru-RU', {timeZone: 'Europe/Moscow'}));
-console.log('Timestamp:', Date.now());
-console.log('Время матча 19:30:', new Date(1759163400000).toLocaleString('ru-RU', {timeZone: 'Europe/Moscow'}));
-console.log('Разница в часах:', (1759163400000 - Date.now()) / (1000 * 60 * 60));
